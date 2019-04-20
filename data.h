f6408a69461c2d06e683b4c0ec604b70839636a7
sirfoga <sirfoga@protonmail.com>
2019-04-20 09:18:30 +0200

    fix name

--- data.h
+++ data.h
@@ -8,25 +8,24 @@
 #define MAX_CAN_ID 800
 #define CAN_MSG_SIZE 8
 
+// callbacks
+Scheduler runner;
+
 void write2SDCallback();
 void save2SDCallback();
 
 Task tWrite2SD(20, TASK_FOREVER, &write2SDCallback);  // write to SD file each 20 ms
 Task tSave2SD(5 * TASK_SECOND, TASK_FOREVER, &save2SDCallback);  // save to SD each 5 s
 
-Scheduler runner;
-
-const int chipSelect = 87; // SD pin
-
 // CAN data logging
 uint8_t CAN_DATA[MAX_CAN_ID][8];
+CAN_FRAME incoming;  // read CAN data into this
 
+// SD
+const int chipSelect = 87; // SD pin
 String header;  // header of log file
-char fileName[16]; // SDcard uses 8.3 names so 16 bytes is enough NOte room for the '\0' char is needed!
-
+char fileName[16];  // SDcard uses 8.3 names so 16 bytes is enough NOte room for the '\0' char is needed!
 unsigned int nr = 1;  // # of log file
 File dataFile; // to save in SD
-
-CAN_FRAME incoming;  // read CAN data into this
 
 #endif // DATA_H
